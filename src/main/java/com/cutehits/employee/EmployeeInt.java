e0edeb33fc3c0044ce4cc7b0fcf3158333e57397
cshekhargit <shekharmca2005@gmail.com>
2019-04-08 22:34:05 +0530

    Update EmployeeInt.java

--- src/main/java/com/cutehits/employee/EmployeeInt.java
+++ src/main/java/com/cutehits/employee/EmployeeInt.java
@@ -1,6 +1,7 @@
 package com.cutehits.employee;
 
-
+import org.springframework.stereotype.Repository;
+@Repository
 import org.springframework.data.repository.CrudRepository;
 
 public interface EmployeeInt extends CrudRepository<Employee, Long>  {
