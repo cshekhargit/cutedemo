be6e7222b1fd25866c78c49d079ef91a0c055cc2
Chandra Shekhar Pandey <shekharmca2005@gmail.com>
2019-05-04 12:43:03 +0530

    added logger

--- src/main/java/com/cutehits/cutedemo/HelloWorldController.java
+++ src/main/java/com/cutehits/cutedemo/HelloWorldController.java
@@ -1,14 +1,46 @@
 package com.cutehits.cutedemo;
+import java.util.logging.LogManager;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.io.Resource;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+
 
 @RestController
 
 public class HelloWorldController {
 
+	//public static final Logger log = LoggerFactory.getLogger(Resource.class);
+	
+	//private static final Logger LOGGER = LogManager.getLogger(TheClass.class);
+	
+	protected static final Logger log = LoggerFactory
+			.getLogger(Resource.class);
+	
 	@RequestMapping("/helloworld")
 	public String  HelloWorld()
 	{
+		log.info("created a new item named  identifier");
+		log.error("created a new item named  identifier");
+        log.warn("created a new item named  identifier");
 		return "Hello World";
 	}
+	
+	/*@RequestMapping("/error")
+	String logger() {
+		
+		log.info("created a new item named  identifier");
+		log.error("created a new item named  identifier");
+        log.warn("created a new item named  identifier");
+
+        System.out.println("Test");
+       
+        return "Logger Called.";
+
+    }*/
+
 }
