0ff228aa7a2cbbf8b22eeb08d7c1f31b87a11faa
Chandra Shekhar Pandey <Cpandey@vidyo.com>
2019-05-06 14:49:01 +0530

    added new test case

---
+++ src/test/java/com/cutehits/cutedemo/MyTest.java
@@ -0,0 +1,15 @@
+package com.cutehits.cutedemo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MyTest {
+
+	@Test
+	public void test() {
+		//fail("Not yet implemented");
+		assertTrue(true);
+	}
+
+}
